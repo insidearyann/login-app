3404597da8c289fc2b8f33c2a8b0c57ead54a7b1
Aryan Tripathi <aryantripathi@Aryans-MacBook-Air-3.local>
2026-04-22 22:47:43 +0530

    Initial clean project

---
+++ src/main/java/com/example/Login/Controller/AuthController.java
@@ -0,0 +1,18 @@
+package com.example.Login.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+}
